6aa3b26d904b19dec9fdff5983dd6dc0cd34851d
Anubhav Pabby <anubhavpabby@gmail.com>
2020-11-07 16:10:18 +0530

    Added A Another Red Coloured Dice with White Dice which also generates random dice faces with the white dice based on Random Number Generation

--- app/src/main/java/com/anubhavpabby/diceroller/MainActivity.kt
+++ app/src/main/java/com/anubhavpabby/diceroller/MainActivity.kt
@@ -9,31 +9,49 @@ import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
 
-    var diceImage:ImageView? = null
+    var whiteDiceImage:ImageView? = null
+    var redDiceImage:ImageView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        diceImage = findViewById(R.id.dice_img)
+        whiteDiceImage = findViewById(R.id.dice_img_white)
+        redDiceImage = findViewById(R.id.dice_img_red)
         val rollButton:Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener {
-            rollDice()
+            rollWhiteDice()
+            rollRedDice()
         }
     }
 
-    private fun rollDice(){
+    private fun rollWhiteDice(){
         val randomInt = Random.nextInt(6) + 1
         val drawableImageRandomId = when(randomInt){
-            1 -> R.drawable.dice_1
-            2 -> R.drawable.dice_2
-            3 -> R.drawable.dice_3
-            4 -> R.drawable.dice_4
-            5 -> R.drawable.dice_5
-            6 -> R.drawable.dice_6
+            1 -> R.drawable.white_dice_1
+            2 -> R.drawable.white_dice_2
+            3 -> R.drawable.white_dice_3
+            4 -> R.drawable.white_dice_4
+            5 -> R.drawable.white_dice_5
+            6 -> R.drawable.white_dice_6
             else -> R.drawable.empty_dice
         }
 
-        diceImage!!.setImageResource(drawableImageRandomId)
+        whiteDiceImage!!.setImageResource(drawableImageRandomId)
+    }
+
+    private fun rollRedDice(){
+        val randomInt = Random.nextInt(6) + 1
+        val drawableImageRandomId = when(randomInt){
+            1 -> R.drawable.red_dice_1
+            2 -> R.drawable.red_dice_2
+            3 -> R.drawable.red_dice_3
+            4 -> R.drawable.red_dice_4
+            5 -> R.drawable.red_dice_5
+            6 -> R.drawable.red_dice_6
+            else -> R.drawable.empty_dice
+        }
+
+        redDiceImage!!.setImageResource(drawableImageRandomId)
     }
 }
